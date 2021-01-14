45f65e5ed532e0394baa8a7380d0c28d5bf164ee
Olenat <olenat@aplit-soft.com>
2021-01-14 15:27:09 +0200

    For checking

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,17 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("lastsample.fxml"));
         primaryStage.setTitle("Dm5 Tester");
-        Scene scene = new Scene(root, 1920, 1080);
+        Scene scene = new Scene(root, 1500, 800);
         scene.getStylesheets().add("style.css");
+
         primaryStage.setScene(scene);
+        //primaryStage.setFullScreen(true);
+        primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
