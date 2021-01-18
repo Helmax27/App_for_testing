a91c5ca7c6e1c286aac2edde379e1ec23c6ca5f5
Olenat <olenat@aplit-soft.com>
2021-01-18 19:02:40 +0200

    18/01/21 changes add Controller

---
+++ src/sample/NewsampleController.java
@@ -0,0 +1,105 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class NewsampleController {
+    @FXML
+    public BorderPane borderPane;
+    @FXML
+    public MenuBar menuBar;
+    @FXML
+    public Menu testItem;
+    @FXML
+    public MenuItem exitItem;
+    @FXML
+    public MenuItem startItem;
+    @FXML
+    public Menu viewItem;
+    @FXML
+    public MenuItem showLogItem;
+    @FXML
+    public MenuItem showDescriptionItem;
+    @FXML
+    public Menu settingItem;
+    @FXML
+    public MenuItem profilesItem;
+    @FXML
+    public MenuItem parametersItem;
+    @FXML
+    public MenuItem loopSettingItem;
+    @FXML
+    public Menu helpItem;
+    @FXML
+    public MenuItem aboutItem;
+    @FXML
+    public Button startButton;
+    @FXML
+    public Button stopButton;
+    @FXML
+    public AnchorPane anPanStop;
+    @FXML
+    public ImageView imStop;
+    @FXML
+    public Button viewButton;
+    @FXML
+    public Button updateButton;
+    @FXML
+    public Label profileListLable;
+    @FXML
+    public Button profile1Button;
+    @FXML
+    public Button profile2Button;
+    @FXML
+    public Button profile3Button;
+    @FXML
+    public Button profile4Button;
+    @FXML
+    public TableView tableView;
+
+    @FXML
+    private void onClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void exitAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void startAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void showLogAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void showDescriptionAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void profileAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void parametersAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void loopSettingaction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void helpAction(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void aboutAction(ActionEvent actionEvent) {
+    }
+}
